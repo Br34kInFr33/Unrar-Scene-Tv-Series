459c2f7f6a7d4afadd68afd36164fa2653e322c6
Br34kInFr33 <62271814+Br34kInFr33@users.noreply.github.com>
2021-08-18 23:03:37 -0500

    Update unrar.php

--- unrar.php
+++ unrar.php
@@ -1,5 +1,15 @@
 <?php
 
-//TODO
-
+// Place your directory path to your tv series. - Example - /home/tv/The.Blacklist/ leave the three /* there.  So it would look like this /home/tv/The.Blacklist/*/*/*
+	 $files = glob('/home/jackdani/data/plex/test/Legend.Of.The.Seeker/*/*/*.{r*}', GLOB_BRACE);
+	 foreach ($files as $file) {
+	 	$directory = pathinfo($file, PATHINFO_DIRNAME);
+		if (stripos($file,'.r00') !== false) {
+		    $string="unrar e '$file' '$directory' "; 
+			exec($string);
+		} else {
+		    //do nothing
+		}
+		unlink($file);
+    }
 ?>
